f1ce460dfb7c580b1cf9d667e4a0b5f41774bca0
Vlada Khrustaleva <vlada.khrustaleva@bk.ru>
2024-10-27 02:21:38 +0700

    Первое задание

---
+++ permanent.py
@@ -0,0 +1,10 @@
+class Permanent:
+    BUNS = [
+        ("fluorescent bun", 988),
+        ("crater bun", 1255),
+    ]
+
+    INGREDIENT = [
+        (90, "sauce spicy-x", "sauce"),
+        (1337, "meat of the immortal mollusks protostomia", "main"),
+    ]
