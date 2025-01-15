b7048e05838fbce6c02b64a89ce642c8888f2ef2
Andrey Tutunnikov <tt.a.s@yandex.ru>
2025-01-15 17:52:03 +0300

    созданы тесты test_bun созданы тестовые данные в data

---
+++ data.py
@@ -0,0 +1,8 @@
+import praktikum.ingredient_types as it
+
+bun_data = ['Кислая булка', 205]
+ingredients = {1: [it.INGREDIENT_TYPE_FILLING, 'Филе медведя', 950],
+               2: [it.INGREDIENT_TYPE_FILLING, 'Хрустящие касатки', 352],
+               3: [it.INGREDIENT_TYPE_FILLING, 'Мега-салат Пармезанно', 550],
+               4: [it.INGREDIENT_TYPE_SAUCE, 'Соус 1000 галактик', 150],
+               5: [it.INGREDIENT_TYPE_SAUCE, 'Соус космический помидор', 50]}
