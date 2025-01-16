6f04d6c3e1fb405d038ca0a2eb4195995f1fd6d6
Andrey Tutunnikov <tt.a.s@yandex.ru>
2025-01-16 12:17:49 +0300

    созданы тесты test_database

---
+++ tests/test_database.py
@@ -0,0 +1,12 @@
+from praktikum.database import Database
+
+class TestDatabase:
+    def test_available_buns(self):
+        data = Database()
+
+        assert data.available_buns() == data.buns
+
+    def test_available_ingedient(self):
+        data = Database()
+
+        assert data.available_ingredients() == data.ingredients
