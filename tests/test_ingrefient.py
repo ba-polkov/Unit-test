42f96f3addec224aeada51d1dddd4fb9f6819b3c
Andrey Tutunnikov <tt.a.s@yandex.ru>
2025-01-16 13:15:42 +0300

    создан отчет о покрытии

--- tests/test_ingrefient.py
+++ tests/test_ingrefient.py
@@ -4,21 +4,21 @@ from praktikum.ingredient import Ingredient
 class TestIngredient:
     def test_get_type_ingredient(self):
         ingredient = Ingredient(dt.ingredients[1][0],
-                                dt.ingredients[2][1],
+                                dt.ingredients[3][1],
                                 dt.ingredients[5][2])
 
         assert ingredient.get_type() == dt.ingredients[1][0]
 
     def test_get_name_indrefient(self):
         ingredient = Ingredient(dt.ingredients[1][0],
-                                dt.ingredients[2][1],
+                                dt.ingredients[3][1],
                                 dt.ingredients[5][2])
 
-        assert ingredient.get_name() == dt.ingredients[2][1]
+        assert ingredient.get_name() == dt.ingredients[3][1]
 
     def test_get_price_ingredient(self):
         ingredient = Ingredient(dt.ingredients[1][0],
-                                dt.ingredients[2][1],
+                                dt.ingredients[3][1],
                                 dt.ingredients[5][2])
 
         assert ingredient.get_price() == dt.ingredients[5][2]
