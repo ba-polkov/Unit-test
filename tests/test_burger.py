7c2729c0649271f6735f7d6534d0e1233e7f5c47
Alex Kravchenko <Morines2320@yandex.ru>
2025-02-10 09:07:38 +0300

    fix the name of the classes.

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -7,66 +7,66 @@ from practikum.constants import TB_NAME_BUN, TB_ING_CHEESE_NAME, TB_ING_CHEESE_P
 from practikum.ingredient import Ingredient
 from practikum.ingredient_types import INGREDIENT_TYPE_FILLING, INGREDIENT_TYPE_SAUCE
 
+class TestBurder:
+    # Создаем фикстуру для тестовых объектов булочек и ингредиентов
+    @pytest.fixture
+    def setup_burger(self):
+        bun = Bun(TB_NAME_BUN, TB_PRICE_BUN)
+        cheese = Ingredient(INGREDIENT_TYPE_FILLING, TB_ING_CHEESE_NAME, TB_ING_CHEESE_PRICE)
+        sauce = Ingredient(INGREDIENT_TYPE_SAUCE, TB_ING_SAUSE_NAME, TB_ING_SAUSE_PEICE)
+        cutlet = Ingredient(INGREDIENT_TYPE_FILLING, TB_ING_CUTLET_NAME, TB_ING_CUTLET_PRICE)
 
-# Создаем фикстуру для тестовых объектов булочек и ингредиентов
-@pytest.fixture
-def setup_burger():
-    bun = Bun(TB_NAME_BUN, TB_PRICE_BUN)
-    cheese = Ingredient(INGREDIENT_TYPE_FILLING, TB_ING_CHEESE_NAME, TB_ING_CHEESE_PRICE)
-    sauce = Ingredient(INGREDIENT_TYPE_SAUCE, TB_ING_SAUSE_NAME, TB_ING_SAUSE_PEICE)
-    cutlet = Ingredient(INGREDIENT_TYPE_FILLING, TB_ING_CUTLET_NAME, TB_ING_CUTLET_PRICE)
+        burger = Burger()
+        burger.set_buns(bun)
+        return burger, bun, cheese, cutlet, sauce
 
-    burger = Burger()
-    burger.set_buns(bun)
-    return burger, bun, cheese, cutlet, sauce
+    #тест метода set_buns() для установки булочек
+    def test_set_buns(self, setup_burger):
+        burger, bun, cheese, cutlet, sauce = setup_burger
+        assert burger.bun.get_name() == TB_NAME_BUN, "Метод get_buns() возвращает неверное имя."
+        assert burger.bun.get_price() == TB_PRICE_BUN, "Метод get_buns() возвращает неверную цену."
 
-#тест метода set_buns() для установки булочек
-def test_set_buns(setup_burger):
-    burger, bun, cheese, cutlet, sauce = setup_burger
-    assert burger.bun.get_name() == TB_NAME_BUN, "Метод get_buns() возвращает неверное имя."
-    assert burger.bun.get_price() == TB_PRICE_BUN, "Метод get_buns() возвращает неверную цену."
+    #тест метода add_ingredient() для добавления ингредиентов
+    def test_add_ingredient(self, setup_burger):
+        burger, bun, cheese, cutlet, sauce = setup_burger
+        burger.add_ingredient(cheese)
+        burger.add_ingredient(cutlet)
+        assert len(burger.ingredients) == 2 , "Количество ингредиентов не соответствует проверяемому значению."
+        assert burger.ingredients[0].get_name() == TB_ING_CHEESE_NAME, "Метод add_ingredient() не добавил элемент."
+        assert burger.ingredients[1].get_name() == TB_ING_CUTLET_NAME, "Метод add_ingredient() не добавил элемент."
 
-#тест метода add_ingredient() для добавления ингредиентов
-def test_add_ingredient(setup_burger):
-    burger, bun, cheese, cutlet, sauce = setup_burger
-    burger.add_ingredient(cheese)
-    burger.add_ingredient(cutlet)
-    assert len(burger.ingredients) == 2 , "Количество ингредиентов не соответствует проверяемому значению."
-    assert burger.ingredients[0].get_name() == TB_ING_CHEESE_NAME, "Метод add_ingredient() не добавил элемент."
-    assert burger.ingredients[1].get_name() == TB_ING_CUTLET_NAME, "Метод add_ingredient() не добавил элемент."
-
-#тест для метода  remove_ingredient()
-def test_remove_ingredient(setup_burger):
-    burger, bun, cheese, cutlet, sauce = setup_burger
-    burger.add_ingredient(cheese)
-    burger.add_ingredient(cutlet)
-    burger.remove_ingredient(0)  # Удаляем первый ингредиент (сheese)
-    assert len(burger.ingredients) == 1 , "Количество ингредиентов не соответствует проверяемому значению."
-    assert burger.ingredients[0].get_name() == TB_ING_CUTLET_NAME, "На позиции [0], нет проверяемого элемента."
+    #тест для метода  remove_ingredient()
+    def test_remove_ingredient(self, setup_burger):
+        burger, bun, cheese, cutlet, sauce = setup_burger
+        burger.add_ingredient(cheese)
+        burger.add_ingredient(cutlet)
+        burger.remove_ingredient(0)  # Удаляем первый ингредиент (сheese)
+        assert len(burger.ingredients) == 1 , "Количество ингредиентов не соответствует проверяемому значению."
+        assert burger.ingredients[0].get_name() == TB_ING_CUTLET_NAME, "На позиции [0], нет проверяемого элемента."
 
 
-#тест метода move_ingredient(), который меняет порядок позиций в списке ингредиентов
-def test_move_ingredient_reposition_ing_at_new_position(setup_burger):
-    burger, bun, cheese, cutlet, sauce = setup_burger
-    burger.add_ingredient(cheese)
-    burger.add_ingredient(cutlet)
-    burger.move_ingredient(0, 1)  # Перемещаем Cheese на вторую позицию
-    assert burger.ingredients[0].get_name() == TB_ING_CUTLET_NAME, "На позиции [0], нет проверяемого элемента."
-    assert burger.ingredients[1].get_name() == TB_ING_CHEESE_NAME, "На позиции [1], нет проверяемого элемента."
+    #тест метода move_ingredient(), который меняет порядок позиций в списке ингредиентов
+    def test_move_ingredient_reposition_ing_at_new_position(self, setup_burger):
+        burger, bun, cheese, cutlet, sauce = setup_burger
+        burger.add_ingredient(cheese)
+        burger.add_ingredient(cutlet)
+        burger.move_ingredient(0, 1)  # Перемещаем Cheese на вторую позицию
+        assert burger.ingredients[0].get_name() == TB_ING_CUTLET_NAME, "На позиции [0], нет проверяемого элемента."
+        assert burger.ingredients[1].get_name() == TB_ING_CHEESE_NAME, "На позиции [1], нет проверяемого элемента."
 
-#тест метода get_price(), который считает цену бургера
-def test_get_price_fill_burger_expected_price_is_valid(setup_burger):
-    burger, bun, cheese, cutlet, sauce = setup_burger
-    burger.add_ingredient(cheese)
-    burger.add_ingredient(cutlet)
-    burger.add_ingredient(sauce)
-    expected_price = bun.get_price() * 2 + cheese.get_price() + cutlet.get_price() + sauce.get_price()
-    assert burger.get_price() == expected_price, "Результат не соответствует ожидаемому."
+    #тест метода get_price(), который считает цену бургера
+    def test_get_price_fill_burger_expected_price_is_valid(self, setup_burger):
+        burger, bun, cheese, cutlet, sauce = setup_burger
+        burger.add_ingredient(cheese)
+        burger.add_ingredient(cutlet)
+        burger.add_ingredient(sauce)
+        expected_price = bun.get_price() * 2 + cheese.get_price() + cutlet.get_price() + sauce.get_price()
+        assert burger.get_price() == expected_price, "Результат не соответствует ожидаемому."
 
- #тест метода get_receipt(), который выводит состав бургера и его цену
-def test_get_receipt_fill_burger_expected_result_is_valid(setup_burger):
-    burger, bun, cheese, cutlet, sauce = setup_burger
-    burger.add_ingredient(cheese)
-    burger.add_ingredient(cutlet)
-    burger.add_ingredient(sauce)
-    assert burger.get_receipt() == EXPECTED_RECEIPT, "Результат не соответствует ожидаемому."
+     #тест метода get_receipt(), который выводит состав бургера и его цену
+    def test_get_receipt_fill_burger_expected_result_is_valid(self, setup_burger):
+        burger, bun, cheese, cutlet, sauce = setup_burger
+        burger.add_ingredient(cheese)
+        burger.add_ingredient(cutlet)
+        burger.add_ingredient(sauce)
+        assert burger.get_receipt() == EXPECTED_RECEIPT, "Результат не соответствует ожидаемому."
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -3,28 +3,28 @@ from practikum.ingredient import Ingredient
 from practikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 from practikum.constants import TB_ING_SAUSE_PEICE, TB_ING_SAUSE_NAME, TB_ING_CHEESE_NAME, TB_ING_CHEESE_PRICE, H_INGREDIENTS
 
+class TestIngredient:
+    # Тестируем получение цены ингредиента
+    def test_get_price(self):
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, TB_ING_SAUSE_NAME, TB_ING_SAUSE_PEICE)
+        assert ingredient.get_price() == TB_ING_SAUSE_PEICE, "Метод get_price() возвращает неверную цену."
 
-# Тестируем получение цены ингредиента
-def test_get_price():
-    ingredient = Ingredient(INGREDIENT_TYPE_FILLING, TB_ING_SAUSE_NAME, TB_ING_SAUSE_PEICE)
-    assert ingredient.get_price() == TB_ING_SAUSE_PEICE, "Метод get_price() возвращает неверную цену."
+    # Тестируем получение имени ингредиента
+    def test_get_name(self):
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, TB_ING_SAUSE_NAME, TB_ING_SAUSE_PEICE)
+        assert ingredient.get_name() == TB_ING_SAUSE_NAME, "Метод get_name() возвращает неверное имя."
 
-# Тестируем получение имени ингредиента
-def test_get_name():
-    ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, TB_ING_SAUSE_NAME, TB_ING_SAUSE_PEICE)
-    assert ingredient.get_name() == TB_ING_SAUSE_NAME, "Метод get_name() возвращает неверное имя."
-
-# Тестируем получение типа ингредиента
-def test_get_type():
-    ingredient = Ingredient(INGREDIENT_TYPE_FILLING, TB_ING_CHEESE_NAME, TB_ING_CHEESE_PRICE)
-    assert ingredient.get_type() == INGREDIENT_TYPE_FILLING, "Метод get_type() возвращает неверный тип."
+    # Тестируем получение типа ингредиента
+    def test_get_type(self):
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, TB_ING_CHEESE_NAME, TB_ING_CHEESE_PRICE)
+        assert ingredient.get_type() == INGREDIENT_TYPE_FILLING, "Метод get_type() возвращает неверный тип."
 
 
-# Используем параметризацию для тестирования создания ингредиентов
-@pytest.mark.parametrize("ingredient_type, name, price", H_INGREDIENTS)
-def test_ingredient_parametrized_creation(ingredient_type, name, price):
-    ingredient = Ingredient(ingredient_type, name, price)
-    assert ingredient.get_price() == price, "Метод get_price() возвращает неверную цену."
-    assert ingredient.get_name() == name, "Метод get_name() возвращает неверное имя."
-    assert ingredient.get_type() == ingredient_type, "Метод get_type() возвращает неверный тип."
+    # Используем параметризацию для тестирования создания ингредиентов
+    @pytest.mark.parametrize("ingredient_type, name, price", H_INGREDIENTS)
+    def test_ingredient_parametrized_creation(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_price() == price, "Метод get_price() возвращает неверную цену."
+        assert ingredient.get_name() == name, "Метод get_name() возвращает неверное имя."
+        assert ingredient.get_type() == ingredient_type, "Метод get_type() возвращает неверный тип."
 
