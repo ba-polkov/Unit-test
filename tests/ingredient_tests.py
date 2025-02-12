2b42bc78e5926b68a3b06b9f1955f72016d51fcf
Andrey Ch. <a.cherremiskin@bk.ru>
2025-02-12 11:34:24 +0300

    la finalle

--- tests/ingredient_tests.py
+++ tests/ingredient_tests.py
@@ -8,8 +8,21 @@ class TestIngredient:
     @pytest.mark.parametrize('ingredient_type, name, price',
                              ((Data.ingr_2[0], Data.ingr_2[1], Data.ingr_2[2]),
                               (Data.ingr_3[0], Data.ingr_3[1], Data.ingr_3[2])))
-    def test_ingredients(self, ingredient_type, name, price):
+    def test_ingredients_type(self, ingredient_type, name, price):
         ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_type() == ingredient_type
+
+
+    @pytest.mark.parametrize('ingredient_type, name, price',
+                             ((Data.ingr_2[0], Data.ingr_2[1], Data.ingr_2[2]),
+                              (Data.ingr_3[0], Data.ingr_3[1], Data.ingr_3[2])))
+    def test_ingredients_name(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_name() == name
+
+    @pytest.mark.parametrize('ingredient_type, name, price',
+                             ((Data.ingr_2[0], Data.ingr_2[1], Data.ingr_2[2]),
+                              (Data.ingr_3[0], Data.ingr_3[1], Data.ingr_3[2])))
+    def test_ingredients_price(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
         assert ingredient.get_price() == price
