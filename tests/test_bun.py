59b4e78981500a86f4829b7014475a3570e523b3
gorbunova_is <gorbunova_is@magnit.ru>
2025-02-12 12:30:14 +0300

    commit fix test_bun (убрала лишний вызов метода)

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -8,7 +8,6 @@ class TestBun:
     def test_get_name_bun(self, name_bun):
         bun = Bun(name_bun, 99.9)
         assert bun.get_name() == name_bun
-        bun.get_price()
 
     @pytest.mark.parametrize("price_bun", [99.9, 100, 1000000.1, 1.000001])
     def test_get_price_bun(self, price_bun):
