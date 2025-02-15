0bb53f9e34b5e03cf56fa5b5b08f6df07e83e953
Vladislav Semenov <stori.semen@yandex.ru>
2025-02-15 19:02:36 +0300

    Добавлен файл с внешними зависимостями.

--- tests/test_class_bun.py
+++ tests/test_class_bun.py
@@ -1,13 +1,10 @@
 
 class TestBun:
 
-
-# Тест для проверки метода get_name
     def test_get_name(self, default_bun):
         bun_name, _ = default_bun.get_name(), default_bun.get_price()
         assert default_bun.get_name() == bun_name
 
-# Тест для проверки метода get_price
     def test_get_price(self, default_bun):
         _, bun_price = default_bun.get_name(), default_bun.get_price()
         assert default_bun.get_price() == bun_price
