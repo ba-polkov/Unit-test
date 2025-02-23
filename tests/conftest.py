aa442846cfaaa3670cb30e2c15ec6186c65eb0bf
Diana Nigmatullina <94nigma@gmail.com>
2025-02-23 21:57:48 +0200

    added pytest-cov

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock
 from praktikum.burger import Burger
 from praktikum.bun import Bun
 from data import Data
+from praktikum.ingredient import Ingredient
 
 
 @pytest.fixture
@@ -13,3 +14,23 @@ def burger():
     bun.get_price.return_value = Data.BUNS[0][1]
     burger.set_buns(bun)
     return burger
+
+
+
+@pytest.fixture
+def ingredient1():
+    ingredient_type, ingredient_name, ingredient_price = Data.INGREDIENTS[0]
+    ingredient = MagicMock(spec=Ingredient)
+    ingredient.get_name.return_value = ingredient_name
+    ingredient.get_price.return_value = ingredient_price
+    ingredient.get_type.return_value = ingredient_type
+    return ingredient
+
+@pytest.fixture
+def ingredient2():
+    ingredient_type, ingredient_name, ingredient_price = Data.INGREDIENTS[1]
+    ingredient = MagicMock(spec=Ingredient)
+    ingredient.get_name.return_value = ingredient_name
+    ingredient.get_price.return_value = ingredient_price
+    ingredient.get_type.return_value = ingredient_type
+    return ingredient
--- tests/test_burger.py
+++ tests/test_burger.py
@@ -4,78 +4,86 @@ from praktikum.ingredient import Ingredient
 from data import Data
 
 
-def test_set_buns(burger):
+
+
+def test_bun_name(burger):
     assert burger.bun.get_name() == Data.BUNS[0][0]
+
+def test_bun_price(burger):
     assert burger.bun.get_price() == Data.BUNS[0][1]
 
 
 @pytest.mark.parametrize("ingredient_type, ingredient_name, price", Data.INGREDIENTS)
-def test_add_ingredient(burger, ingredient_type, ingredient_name, price):
-    ingredient = MagicMock(spec=Ingredient)
-    ingredient.get_name.return_value = ingredient_name
-    ingredient.get_price.return_value = price
-    ingredient.get_type.return_value = ingredient_type  # Устанавливаем тип ингредиента
-
+def test_add_ingredient(burger, ingredient1, ingredient_type, ingredient_name, price):
+    ingredient1.get_name.return_value = ingredient_name
+    ingredient1.get_price.return_value = price
+    ingredient1.get_type.return_value = ingredient_type  # Устанавливаем тип ингредиента
     # Добавляем ингредиент в бургер
-    burger.add_ingredient(ingredient)
-
+    burger.add_ingredient(ingredient1)
     # Проверяем, что ингредиент добавлен
     assert len(burger.ingredients) == 1
+
+@pytest.mark.parametrize("ingredient_type, ingredient_name, price", Data.INGREDIENTS)
+def test_add_ingredient_name(burger, ingredient1, ingredient_type, ingredient_name, price):
+    ingredient1.get_name.return_value = ingredient_name
+    ingredient1.get_price.return_value = price
+    ingredient1.get_type.return_value = ingredient_type  # Устанавливаем тип ингредиента
+    # Добавляем ингредиент в бургер
+    burger.add_ingredient(ingredient1)
+    # Проверяем, что имя ингредиента правильно
     assert burger.ingredients[0].get_name() == ingredient_name
+
+@pytest.mark.parametrize("ingredient_type, ingredient_name, price", Data.INGREDIENTS)
+def test_add_ingredient_price(burger, ingredient1, ingredient_type, ingredient_name, price):
+    ingredient1.get_name.return_value = ingredient_name
+    ingredient1.get_price.return_value = price
+    ingredient1.get_type.return_value = ingredient_type  # Устанавливаем тип ингредиента
+    # Добавляем ингредиент в бургер
+    burger.add_ingredient(ingredient1)
+    # Проверяем, что цена ингредиента правильна
     assert burger.ingredients[0].get_price() == price
+
+@pytest.mark.parametrize("ingredient_type, ingredient_name, price", Data.INGREDIENTS)
+def test_add_ingredient_type(burger, ingredient1, ingredient_type, ingredient_name, price):
+    ingredient1.get_name.return_value = ingredient_name
+    ingredient1.get_price.return_value = price
+    ingredient1.get_type.return_value = ingredient_type  # Устанавливаем тип ингредиента
+    # Добавляем ингредиент в бургер
+    burger.add_ingredient(ingredient1)
     assert burger.ingredients[0].get_type() == ingredient_type  # Проверяем тип ингредиента
 
 
-def test_move_ingredient(burger):
-    # Добавляем два ингредиента
-    ingredient1_type, ingredient1_name, ingredient1_price = Data.INGREDIENTS[0]  # hot sauce, 100, SAUCE
-    ingredient2_type, ingredient2_name, ingredient2_price = Data.INGREDIENTS[1]  # cutlet, 200, FILLING
-    ingredient1 = MagicMock(spec=Ingredient)
-    ingredient2 = MagicMock(spec=Ingredient)
-    ingredient1.get_name.return_value = ingredient1_name
-    ingredient1.get_price.return_value = ingredient1_price
-    ingredient1.get_type.return_value = ingredient1_type
-    ingredient2.get_name.return_value = ingredient2_name
-    ingredient2.get_price.return_value = ingredient2_price
-    ingredient2.get_type.return_value = ingredient2_type
 
+
+def test_move_ingredient(burger, ingredient1, ingredient2):
+    # Добавляем два ингредиента
     burger.add_ingredient(ingredient1)
     burger.add_ingredient(ingredient2)
-
     # Перемещаем ингредиенты
     burger.move_ingredient(0, 1)
-
     # Проверяем, что ингредиенты перемещены
-    assert burger.ingredients[0].get_name() == ingredient2_name
-    assert burger.ingredients[1].get_name() == ingredient1_name
+    assert burger.ingredients[0].get_name() == ingredient2.get_name() and burger.ingredients[1].get_name() == ingredient1.get_name()
 
 
-def test_remove_ingredient(burger):
+
+def test_remove_ingredient(burger, ingredient2):
     # Добавляем ингредиент
-    ingredient_type, ingredient_name, price = Data.INGREDIENTS[1]
-    ingredient = MagicMock(spec=Ingredient)
-    ingredient.get_name.return_value = ingredient_name
-    ingredient.get_price.return_value = price
-    ingredient.get_type.return_value = ingredient_type
-    burger.add_ingredient(ingredient)
-
+    burger.add_ingredient(ingredient2)
     # Удаляем ингредиент
     burger.remove_ingredient(0)
-
     # Проверяем, что ингредиент удалён
     assert len(burger.ingredients) == 0
 
 
-def test_get_price_with_ingredient(burger):
-    ingredient_type, ingredient_name, price = Data.INGREDIENTS[1]
-    ingredient = MagicMock(spec=Ingredient)
-    ingredient.get_price.return_value = price
-    ingredient.get_type.return_value = ingredient_type
-    burger.add_ingredient(ingredient)
 
+def test_get_price_with_ingredient(burger, ingredient2):
+    # Добавляем ингредиент
+    burger.add_ingredient(ingredient2)
     # Проверяем общую цену
-    expected_price = Data.BUNS[0][1] * 2 + price
+    expected_price = Data.BUNS[0][1] * 2 + ingredient2.get_price()
     assert burger.get_price() == expected_price
+
+
 
 
 def test_get_price_without_ingredient(burger):
@@ -83,28 +91,14 @@ def test_get_price_without_ingredient(burger):
     assert burger.get_price() == Data.BUNS[0][1] * 2
 
 
-def test_get_receipt(burger):
-    # Создаем и настраиваем ингредиенты
-    ingredient1_type, ingredient1_name, ingredient1_price = Data.INGREDIENTS[0]
-    ingredient2_type, ingredient2_name, ingredient2_price = Data.INGREDIENTS[1]
 
-    ingredient1 = MagicMock(spec=Ingredient)
-    ingredient1.get_name.return_value = ingredient1_name
-    ingredient1.get_price.return_value = ingredient1_price
-    ingredient1.get_type.return_value = ingredient1_type
 
-    ingredient2 = MagicMock(spec=Ingredient)
-    ingredient2.get_name.return_value = ingredient2_name
-    ingredient2.get_price.return_value = ingredient2_price
-    ingredient2.get_type.return_value = ingredient2_type
-
-    # Добавляем ингредиенты в бургер
+def test_get_receipt(burger, ingredient1, ingredient2):
+    # Добавляем ингредиенты в бурге
     burger.add_ingredient(ingredient1)
     burger.add_ingredient(ingredient2)
-
     # Получаем чек
     receipt = burger.get_receipt()
-
     # Ожидаемый результат
     expected_receipt = (
         f'(==== {Data.BUNS[0][0]} ====)\n'
