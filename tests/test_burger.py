d6d4f476a8df45a34e974d82a09385270be0f13a
Izam Agabalaev <izamagabalaev592@gmail.com>
2025-02-24 13:15:15 +0500

    Изменил название тестовых методов.

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -5,7 +5,7 @@ from conftest import *
 class TestBurger():
 
     @allure.title('Проверка метода set_buns')
-    def test_set_buns(self, mock_bun_one):
+    def test_set_buns_works_correctly(self, mock_bun_one):
         burger = Burger()
         burger.set_buns(mock_bun_one)
         assert burger.bun == mock_bun_one
@@ -16,7 +16,7 @@ class TestBurger():
                             [DataOne.FILLING_NAME, DataOne.FILLING_NAME],
                             [DataTwo.FILLING_NAME, DataTwo.FILLING_NAME]
     ])
-    def test_add_ingredient (self, ingredients, added_ingredients):
+    def test_add_ingredient_works_correctly (self, ingredients, added_ingredients):
         burger = Burger()
         burger.add_ingredient(ingredients)
         assert burger.ingredients == [added_ingredients] and len(burger.ingredients) == 1
@@ -26,7 +26,7 @@ class TestBurger():
                             [DataOne.SAUCE_NAME, DataOne.SAUCE_NAME],
                             [DataTwo.FILLING_NAME, DataTwo.FILLING_NAME]
     ])
-    def test_remove_ingredient(self, ingredients, remove_ingredients):
+    def test_remove_ingredient_works_correctly(self, ingredients, remove_ingredients):
         burger = Burger()
         burger.add_ingredient(mock_filling_one)
         burger.add_ingredient(ingredients)
@@ -34,7 +34,7 @@ class TestBurger():
         assert remove_ingredients not in burger.ingredients and mock_filling_one in burger.ingredients
 
     @allure.title('Проверка метода move_ingredient')
-    def test_move_ingredient(self, mock_sauce_one, mock_filling_one):
+    def test_move_ingredient_works_correctly(self, mock_sauce_one, mock_filling_one):
         burger = Burger()
         burger.add_ingredient(mock_sauce_one)
         burger.add_ingredient(mock_filling_one)
@@ -44,7 +44,7 @@ class TestBurger():
         assert burger.ingredients[1] == mock_sauce_one
 
     @allure.title('Проверка метода get_price')
-    def test_get_price(self, mock_bun_two, mock_sauce_two, mock_filling_two):
+    def test_get_price_burger_works_correctly(self, mock_bun_two, mock_sauce_two, mock_filling_two):
         burger = Burger()
         burger.set_buns(mock_bun_two)
         burger.add_ingredient(mock_sauce_two)
@@ -52,7 +52,7 @@ class TestBurger():
         assert burger.get_price() == DataTwo.BURGER_PRICE
 
     @allure.title('get_receipt')
-    def test_get_receipt(self, mock_bun_one, mock_sauce_one, mock_filling_one, mock_filling_two):
+    def test_get_receipt_burger_works_correctly(self, mock_bun_one, mock_sauce_one, mock_filling_one, mock_filling_two):
         burger = Burger()
         burger.set_buns(mock_bun_one)
         burger.add_ingredient(mock_sauce_one)
--- tests/test_database.py
+++ tests/test_database.py
@@ -7,14 +7,14 @@ class TestDataBase:
 
     @allure.title('Проверка метода get_type')
     @pytest.mark.parametrize('index_bun, bun_name, bun_price', DataBase.database_buns)
-    def test_available_buns(self, data_base, index_bun, bun_name, bun_price):
+    def test_available_buns_works_correctly(self, data_base, index_bun, bun_name, bun_price):
         data_buns = data_base.available_buns()
         assert data_buns[index_bun].get_name() == bun_name
         assert data_buns[index_bun].get_price() == bun_price
 
     @allure.title('Проверка метода available_ingredients')
     @pytest.mark.parametrize('index_ingredient, type_ingredient, name_ingredient, price_ingredient', DataBase.database_ingredients)
-    def test_available_ingredients(self, data_base, type_ingredient, index_ingredient, name_ingredient, price_ingredient):
+    def test_available_ingredients_works_correctly(self, data_base, type_ingredient, index_ingredient, name_ingredient, price_ingredient):
         data_ingrediets = data_base.available_ingredients()
         assert data_ingrediets[index_ingredient].get_name() == name_ingredient
         assert data_ingrediets[index_ingredient].get_type() == type_ingredient
