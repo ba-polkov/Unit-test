7982816fd84ddde2880ab8b8c46fd301f6d66d6a
Сергей Грабыльников <dgureva96@gmail.com>
2025-03-12 20:50:58 +0300

    Commit_1

---
+++ tests/test_ingredient.py
@@ -0,0 +1,25 @@
+import pytest
+from praktikum.ingredient import Ingredient
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+class TestIngredient:
+    # Проверка корректного сохранения типа ингредиента
+    @pytest.mark.parametrize("type, name, price",
+                             [(INGREDIENT_TYPE_SAUCE, "hot sauce", 100), (INGREDIENT_TYPE_FILLING, "сыр", 70), ], )
+    def test_ingredient_get_type(self, type, name, price):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_type() == type
+
+    # Проверка корректного сохранения имени ингредиента
+    @pytest.mark.parametrize("type, name, price",
+                             [(INGREDIENT_TYPE_SAUCE, "hot sauce", 100), (INGREDIENT_TYPE_FILLING, "сыр", 70), ], )
+    def test_ingredient_get_name(self, type, name, price):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_name() == name
+
+    # Проверка корректного сохранения цены ингредиента
+    @pytest.mark.parametrize("type, name, price",
+                             [(INGREDIENT_TYPE_SAUCE, "hot sauce", 100), (INGREDIENT_TYPE_FILLING, "сыр", 70), ], )
+    def test_ingredient_get_price(self, type, name, price):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_price() == price
