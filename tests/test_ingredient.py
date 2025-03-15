419dbcd984fd4b31a0812cd1032e2468f53fe68f
Roman Glazykin <r0main.tch@yandex.ru>
2025-03-15 20:59:49 +0300

    внесены изменения согласно комментариям ревьюера

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,14 +1,16 @@
+import pytest
 
-def test_ingredient_creation(ketchup):
-    assert ketchup.type == 'SAUCE'
-    assert ketchup.name == 'Кетчуп'
-    assert ketchup.price == 3.50
+class TestIngredient:
+    def test_ingredient_creation(self, ketchup):
+        assert ketchup.type == 'SAUCE'
+        assert ketchup.name == 'Кетчуп'
+        assert ketchup.price == 3.50
 
-def test_get_price_ingredient(ketchup):
-    assert ketchup.get_price() == 3.50
+    def test_get_price_ingredient(self, ketchup):
+        assert ketchup.get_price() == 3.50
 
-def test_get_name_ingredient(ketchup):
-    assert ketchup.get_name() == 'Кетчуп'
+    def test_get_name_ingredient(self, ketchup):
+        assert ketchup.get_name() == 'Кетчуп'
 
-def test_get_type_ingredient(cucumber):
-    assert cucumber.type == 'FILLING'
+    def test_get_type_ingredient(self, cucumber):
+        assert cucumber.type == 'FILLING'
