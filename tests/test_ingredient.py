9d775602d5831d4dd5523d131bf85a0210ec2859
Дарья <dgureva96@gmail.com>
2025-03-16 14:49:49 +0300

    Update test_ingredient.py

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -3,21 +3,18 @@ from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 class TestIngredient:
-    # Проверка корректного сохранения типа ингредиента
     @pytest.mark.parametrize("type, name, price",
                              [(INGREDIENT_TYPE_SAUCE, "hot sauce", 100), (INGREDIENT_TYPE_FILLING, "cutlet", 100), ], )
     def test_ingredient_get_type(self, type, name, price):
         ingredient = Ingredient(type, name, price)
         assert ingredient.get_type() == type
 
-    # Проверка корректного сохранения имени ингредиента
     @pytest.mark.parametrize("type, name, price",
                              [(INGREDIENT_TYPE_SAUCE, "hot sauce", 100), (INGREDIENT_TYPE_FILLING, "cutlet", 100), ], )
     def test_ingredient_get_name(self, type, name, price):
         ingredient = Ingredient(type, name, price)
         assert ingredient.get_name() == name
 
-    # Проверка корректного сохранения цены ингредиента
     @pytest.mark.parametrize("type, name, price",
                              [(INGREDIENT_TYPE_SAUCE, "hot sauce", 100), (INGREDIENT_TYPE_FILLING, "cutlet", 100), ], )
     def test_ingredient_get_price(self, type, name, price):
