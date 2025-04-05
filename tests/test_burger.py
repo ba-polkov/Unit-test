868682a1e90aff6af10cd249dd65cdde90f0d366
Gleb <glebkharitonenkov@gmail.com>
2025-04-05 16:34:54 +0300

    проверка полного соответствия test_burger

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -14,30 +14,30 @@ class TestBurger:
         assert len(burger.ingredients) == 1
         assert burger.ingredients[0] == mock_sauce
 
-    def test_remove_ingredient(self, prepared_burger):
-        initial_count = len(prepared_burger.ingredients)
+    def test_remove_ingredient(self, prepared_burger, mock_sauce, mock_filling):
+        initial_ingredients = prepared_burger.ingredients.copy()
         prepared_burger.remove_ingredient(0)
-        assert len(prepared_burger.ingredients) == initial_count - 1
+        assert len(prepared_burger.ingredients) == len(initial_ingredients) - 1
+        assert mock_sauce not in prepared_burger.ingredients
+        assert mock_filling in prepared_burger.ingredients
 
-    def test_move_ingredient(self, prepared_burger):
-        first_ingredient = prepared_burger.ingredients[0]
-        second_ingredient = prepared_burger.ingredients[1]
-
+    def test_move_ingredient(self, prepared_burger, mock_sauce, mock_filling):
         prepared_burger.move_ingredient(0, 1)
-
-        assert prepared_burger.ingredients[0] == second_ingredient
-        assert prepared_burger.ingredients[1] == first_ingredient
+        assert prepared_burger.ingredients == [mock_filling, mock_sauce]
 
     def test_get_price(self, prepared_burger, mock_bun, mock_sauce, mock_filling):
         expected_price = mock_bun.get_price() * 2 + mock_sauce.get_price() + mock_filling.get_price()
         assert prepared_burger.get_price() == expected_price
 
     def test_get_receipt(self, prepared_burger, mock_bun, mock_sauce, mock_filling):
-        receipt = prepared_burger.get_receipt()
-        assert mock_bun.get_name() in receipt
-        assert mock_sauce.get_name() in receipt
-        assert mock_filling.get_name() in receipt
-        assert str(prepared_burger.get_price()) in receipt
+        expected_receipt = (
+            f'(==== {mock_bun.get_name()} ====)\n'
+            f'= sauce {mock_sauce.get_name()} =\n'
+            f'= filling {mock_filling.get_name()} =\n'
+            f'(==== {mock_bun.get_name()} ====)\n'
+            f'\nPrice: {prepared_burger.get_price()}'
+        )
+        assert prepared_burger.get_receipt() == expected_receipt
 
     def test_move_ingredient_invalid_index(self, prepared_burger):
         with pytest.raises(IndexError):
