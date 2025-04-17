83b1b90e657ed0dc85158cb5e4efc805733c0a44
George Morev <morevgeorge@gmail.com>
2025-04-17 14:40:45 +0300

    Изменена структура проекта. Добавлена папка praktikum

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,5 +1,4 @@
-import pytest
-from bun import Bun
+from praktikum.praktikum import Bun
 
 
 def test_bun_creation():
---
+++ tests/test_ingredient.py
@@ -0,0 +1,18 @@
+# tests/test_ingredient.py
+
+from praktikum.praktikum import Ingredient
+
+
+def test_ingredient_get_price():
+    ingredient = Ingredient("SAUCE", "BBQ", 50.0)
+    assert ingredient.get_price() == 50.0, "Метод get_price должен возвращать цену ингредиента"
+
+
+def test_ingredient_get_name():
+    ingredient = Ingredient("FILLING", "Bacon", 70.0)
+    assert ingredient.get_name() == "Bacon", "Метод get_name должен возвращать имя ингредиента"
+
+
+def test_ingredient_get_type():
+    ingredient = Ingredient("SAUCE", "Ketchup", 30.0)
+    assert ingredient.get_type() == "SAUCE", "Метод get_type должен возвращать тип ингредиента"
