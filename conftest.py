db853f89e992106fb86d183fda0324f586e44462
Ser Martin <ser.martinovskiy@yandex.ru>
2025-04-25 18:28:34 +0300

    First_Commit

--- conftest.py
+++ conftest.py
@@ -4,8 +4,8 @@ from data import *
 
 @pytest.fixture
 def mock_bun():
-    mock_bun = Mock()
-    mock_bun.get_name.return_value = Data.Bun_name
-    mock_bun.get_price.return_value = Data.Bun_price
-    return mock_bun
+    mock_buns = Mock()
+    mock_buns.get_name.return_value = Data.Bun_name
+    mock_buns.get_price.return_value = Data.Bun_price
+    return mock_buns
 
--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,1 +1,14 @@
 import allure
+from conftest import *
+
+class TestBun:
+
+    @allure.title('Проверка получения имени булочки, через метод get_name')
+    def test_get_name_bun(self, mock_bun):
+        assert mock_bun.get_name() == Data.Bun_name
+
+    @allure.title('Проверка получения стоимости булочки, через метод get_price')
+    def test_get_price_bun(self, mock_bun):
+        assert mock_bun.get_price() == Data.Bun_price
+
+
