9294426112cd6f75b7588d6b08c038099e445238
Ser Martin <ser.martinovskiy@yandex.ru>
2025-04-28 11:40:28 +0300

    First_Commit

--- data.py
+++ data.py
@@ -1,4 +1,6 @@
 from praktikum.ingredient_types import *
+from praktikum.bun import Bun
+from praktikum.ingredient import Ingredient
 
 
 class Data:
@@ -12,3 +14,17 @@ class Data:
     Filling_type = INGREDIENT_TYPE_FILLING
     Filling_name = 'Верблюжий горб'
     Filling_price = 300
+
+    buns_data_for_test_database = [
+                          [0, 'black bun', 100],
+                          [1, "white bun", 200],
+                          [2, "red bun", 300]
+                                  ]
+    ingredients_data_for_test_database = [
+                    [0, INGREDIENT_TYPE_SAUCE, "hot sauce", 100],
+                    [1, INGREDIENT_TYPE_SAUCE, "sour cream", 200],
+                    [2, INGREDIENT_TYPE_SAUCE, "chili sauce", 300],
+                    [3, INGREDIENT_TYPE_FILLING, "cutlet", 100],
+                    [4, INGREDIENT_TYPE_FILLING, "dinosaur", 200],
+                    [5, INGREDIENT_TYPE_FILLING, "sausage", 300]
+                                         ]
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,5 +1,14 @@
 import allure
 from data import Data
 from conftest import *
+from praktikum.database import Database
+import pytest
+
+
 
 class TestDataBase:
+    @pytest.mark.parametrize('buns_number, buns_name, buns_price', Data.buns_data_for_test_database)
+    def test_avalable_buns_database(self, data_base_fixture,buns_number, buns_name, buns_price):
+        test_buns = data_base_fixture.available_buns()
+        assert test_buns[buns_number].get_name() == buns_name
+        assert test_buns[buns_number].get_price() == buns_price
