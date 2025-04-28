31053e133828ec12f87253b3b2a0bcd0bbc028f2
Ser Martin <ser.martinovskiy@yandex.ru>
2025-04-28 14:58:43 +0300

    First_Commit

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -12,13 +12,28 @@ class TestBurger:
         assert burger_fixture.bun.get_name() == Data.Bun_name
 
     @allure.title('Проверка метода add_ingredient через параметризацию, путем добавления начинок')
-    @pytest.mark.parametrize('ingredient_name, expected_ingridient_added_name',
+    @pytest.mark.parametrize('ingredient_name, expected_ingredient_added_name',
                              [
                                  ('mock_sauce', Data.Sauce_name),
                                  ('mock_filling', Data.Filling_name)
                              ])
-    def test_add_ingredient(self,burger_fixture, request, ingredient_name, expected_ingridient_added_name):
+    def test_add_ingredient(self,burger_fixture, request, ingredient_name, expected_ingredient_added_name):
         ingredient = request.getfixturevalue(ingredient_name)
         bf = burger_fixture
         bf.add_ingredient(ingredient)
-        assert bf.ingredients[0].get_name() == expected_ingridient_added_name
+        assert bf.ingredients[0].get_name() == expected_ingredient_added_name
+
+    @allure.title('роверка метода remove_ingredient через параметризацию, путем добавления и удаления начинок')
+    @pytest.mark.parametrize('ingredient_name, expected_ingridient_removed_name',
+                             [
+                                 ('mock_sauce', Data.Sauce_name),
+                                 ('mock_filling', Data.Filling_name)
+                             ])
+    def test_remove_ingredient(self, burger_fixture, request, ingredient_name, expected_ingridient_removed_name):
+        ingredient = request.getfixturevalue(ingredient_name)
+        bf = burger_fixture
+        bf.add_ingredient(ingredient)
+        assert ingredient in bf.ingredients
+        index = bf.ingredients.index(ingredient)
+        bf.remove_ingredient(index)
+        assert expected_ingridient_removed_name not in [ing.get_name() for ing in bf.ingredients]
