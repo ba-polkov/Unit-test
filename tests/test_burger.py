acfd41f0e80fe951b449d8ea3ae852407247c008
G Sh <conan@yandex.ru>
2025-05-02 22:13:14 +0300

    Сделал четвертый тест для burger

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -4,7 +4,6 @@ import ingredient_types
 
 class TestBurger:
 
-
     def test_set_buns(self, bun, burger):
         burger = burger
         burger.set_buns(bun)
@@ -25,3 +24,17 @@ class TestBurger:
         burger.add_ingredient(ingredient)
         burger.remove_ingredient(1)
         assert len(burger.ingredients) == 1
+
+    def test_move_ingredient(self, burger):
+        burger = burger
+        ingredient = Ingredient(ingredient_types.INGREDIENT_TYPE_FILLING, 'Наполнитель', 50)
+        burger.add_ingredient(ingredient)
+        burger.move_ingredient(1, 0)
+        assert (burger.ingredients[0].type == ingredient_types.INGREDIENT_TYPE_FILLING and
+                burger.ingredients[0].name == 'Наполнитель' and burger.ingredients[0].price == 50)
+
+    def test_get_price(self, burger):
+        burger = burger
+
+        assert burger.get_price() == 1060
+
