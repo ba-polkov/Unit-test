28ad73178092fc2a31d7f96f5f88d7051fe39fc9
Vyacheslav Rudenko <globalnet2078@gmail.com>
2025-05-05 02:12:18 +0300

    Add test_burger.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@
 
 import pytest
 from bun import Bun
+from burger import Burger
+from ingredient import Ingredient
 
 
 @pytest.fixture
@@ -15,3 +17,19 @@ def default_bun():
 ])
 def parametrized_bun(request):
     return Bun(*request.param)
+
+
+@pytest.fixture
+def empty_burger():
+    return Burger()
+
+@pytest.fixture
+def prepared_burger():
+    bun = Bun("Краторная булка N-200i", 1255)
+    ingredient1 = Ingredient("SAUCE", "Соус фирменный Space Sauce", 80)
+    ingredient2 = Ingredient("FILLING", "Биокотлета из марсианской Магнолии", 424)
+    burger = Burger()
+    burger.set_buns(bun)
+    burger.add_ingredient(ingredient1)
+    burger.add_ingredient(ingredient2)
+    return burger
