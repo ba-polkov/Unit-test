8995cb8b48cc45b8090cbeda4c71ef12f7e4d994
George Morev <morevgeorge@gmail.com>
2025-05-07 15:35:46 +0300

    Фикстуры вынесены в conftest

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -5,31 +5,6 @@ from praktikum.bun import Bun
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
-
-@pytest.fixture
-def burger():
-    """Фикстура для создания пустого бургера."""
-    return Burger()
-
-
-@pytest.fixture
-def bun():
-    """Фикстура для создания булочки."""
-    return Bun("black bun", 100)
-
-
-@pytest.fixture
-def ingredient_sauce():
-    """Фикстура для создания соуса."""
-    return Ingredient(INGREDIENT_TYPE_SAUCE, "hot sauce", 50)
-
-
-@pytest.fixture
-def ingredient_filling():
-    """Фикстура для создания начинки."""
-    return Ingredient(INGREDIENT_TYPE_FILLING, "cutlet", 150)
-
-
 @allure.feature("Бургер")
 @allure.story("Установка булочки и добавление ингредиентов")
 class TestBurger:
--- tests/test_database.py
+++ tests/test_database.py
@@ -5,13 +5,6 @@ from praktikum.bun import Bun
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
-
-@pytest.fixture
-def database():
-    """Создаем экземпляр базы данных для тестов."""
-    return Database()
-
-
 @allure.feature("База данных")
 @allure.story("Доступные булочки")
 class TestDatabase:
