de5fa0e4e08e57bbd607da3755e40c6709af9a37
Philipp Aslapov <aslapovfilipp@yandex.ru>
2025-05-19 10:10:53 +0400

    Разделил юнит-тесты в файле test_database.py на каждую проверку

--- test/test_database.py
+++ test/test_database.py
@@ -6,14 +6,26 @@ class TestDatabase:
     def setup_method(self):
         self.database = Database()
 
-    def test_available_buns(self):
+    def test_available_buns_is_list(self):
         buns = self.database.available_buns()
         assert isinstance(buns, list)
-        assert all(isinstance(bun, Bun) for bun in buns)
+
+    def test_available_buns_has_expected_length(self):
+        buns = self.database.available_buns()
         assert len(buns) == 3
 
-    def test_available_ingredients(self):
+    def test_available_buns_are_bun_instances(self):
+        buns = self.database.available_buns()
+        assert all(isinstance(bun, Bun) for bun in buns)
+
+    def test_available_ingredients_is_list(self):
         ingredients = self.database.available_ingredients()
         assert isinstance(ingredients, list)
-        assert all(isinstance(ingredient, Ingredient) for ingredient in ingredients)
+
+    def test_available_ingredients_has_expected_length(self):
+        ingredients = self.database.available_ingredients()
         assert len(ingredients) == 6
+
+    def test_available_ingredients_are_ingredient_instances(self):
+        ingredients = self.database.available_ingredients()
+        assert all(isinstance(ingredient, Ingredient) for ingredient in ingredients)
