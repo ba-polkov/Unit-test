67e12f7fc87fe92df087af9dacdddce4fd0070e7
Sergei Filipchenko <s.e.r.e.g.a.-93@mail.ru>
2025-05-30 14:35:28 +0500

    Add test_ingredient

---
+++ tests/test_ingredient.py
@@ -0,0 +1,28 @@
+from ingredient import Ingredient
+
+
+class TestIngredient:
+
+    def test_get_price(self):
+        types = 'souse'
+        name = 'test'
+        price = 99.9
+        ingredient = Ingredient(types, name, price)
+
+        assert ingredient.get_price() == price
+
+    def test_get_name(self):
+        types = 'souse'
+        name = 'test'
+        price = 99.9
+        ingredient = Ingredient(types, name, price)
+
+        assert ingredient.get_name() == name
+
+    def test_get_type(self):
+        types = 'souse'
+        name = 'test'
+        price = 99.9
+        ingredient = Ingredient(types, name, price)
+
+        assert ingredient.get_type() == types
