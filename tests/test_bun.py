5196d37a11698a8555ca3ebb973322b89b9e3306
Sergei Filipchenko <s.e.r.e.g.a.-93@mail.ru>
2025-06-02 14:32:02 +0500

    Fix

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -3,18 +3,15 @@ from praktikum.bun import Bun
 
 
 class TestBun:
+    list_data = [("black bun", 100.0),
+                 ("white bun", 200.0),
+                 ("", 0.0),
+                 ("1", 0.01),
+                 ("b" * 100, 999.99)]
 
-    class TestBun:
-        @pytest.mark.parametrize(
-            "name, price",[
-                ("black bun", 100.0),
-                ("white bun", 200.0),
-                ("", 0.0),
-                ("1", 0.01),
-                ("b" * 100, 999.99)
-            ]
-        )
-        def test_bun_properties(self, name, price):
+    @pytest.mark.parametrize("name, price", list_data)
+    def test_bun_properties(self, name, price):
             bun = Bun(name, price)
+
             assert bun.get_name() == name
             assert bun.get_price() == price
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,28 +1,19 @@
+import pytest
+
 from praktikum.ingredient import Ingredient
 
 
 class TestIngredient:
+    list_data = [("SAUCE", "hot sauce", 100.0),
+                 ("FILLING", "cutlet", 200.0),
+                 ("FILLING", "", 0.0),
+                 ("SAUCE", "1", 0.01),
+                 ("SAUCE", "c" * 100, 999.99)]
 
-    def test_get_price(self):
-        types = 'souse'
-        name = 'test'
-        price = 99.9
-        ingredient = Ingredient(types, name, price)
-
-        assert ingredient.get_price() == price
-
-    def test_get_name(self):
-        types = 'souse'
-        name = 'test'
-        price = 99.9
-        ingredient = Ingredient(types, name, price)
-
-        assert ingredient.get_name() == name
-
-    def test_get_type(self):
-        types = 'souse'
-        name = 'test'
-        price = 99.9
+    @pytest.mark.parametrize("types, name, price", list_data)
+    def test_ingredients_properties(self, types, name, price):
         ingredient = Ingredient(types, name, price)
 
         assert ingredient.get_type() == types
+        assert ingredient.get_name() == name
+        assert ingredient.get_price() == price
