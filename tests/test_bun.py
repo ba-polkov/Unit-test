659d32c9f337cfbf6047d26a4f1c3c4f3dc81ba9
Albert Shadzhanov <albertm79@yandex.ru>
2025-06-24 16:40:55 +0300

    Fixed for comment of the review

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -2,20 +2,33 @@ import pytest
 from praktikum.bun import Bun
 from data import BUN_TEST_CASES
 
-@pytest.mark.parametrize("name, price", BUN_TEST_CASES)
-def test_bun_creation(name, price):
-    bun = Bun(name, price)
-    assert bun.get_name() == name
-    assert bun.get_price() == price
+class TestBunAttributes:
+    @pytest.mark.parametrize("name, price", BUN_TEST_CASES)
+    def test_bun_name(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_name() == name
 
+    @pytest.mark.parametrize("name, price", BUN_TEST_CASES)
+    def test_bun_price(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_price() == price
 
-def test_bun_with_zero_price():
-    bun = Bun("ZeroPrice", 0)
-    assert bun.get_name() == "ZeroPrice"
-    assert bun.get_price() == 0
+    def test_bun_with_zero_price(self):
+        bun = Bun("ZeroPrice", 0)
+        assert bun.get_name() == "ZeroPrice"
 
+    def test_bun_with_zero_price_value(self):
+        bun = Bun("ZeroPrice", 0)
+        assert bun.get_price() == 0
 
-def test_bun_with_empty_name():
-    bun = Bun("", 10)
-    assert bun.get_name() == ""
-    assert bun.get_price() == 10
+    def test_bun_with_empty_name(self):
+        bun = Bun("", 10)
+        assert bun.get_name() == ""
+
+    def test_bun_with_name(self):
+        bun = Bun("TestName", 10)
+        assert bun.get_name() == "TestName"
+
+    def test_bun_with_price(self):
+        bun = Bun("TestName", 10)
+        assert bun.get_price() == 10
