6900fa6f7d287ddd86e822b2eaf42fc10fbc9631
Albert Shadzhanov <albertm79@yandex.ru>
2025-06-24 16:43:27 +0300

    Fixed for comment of the review

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -2,18 +2,18 @@ import pytest
 from praktikum.ingredient import Ingredient
 from data import INGREDIENT_TEST_CASES
 
+class TestIngredient:
+    @pytest.mark.parametrize("name, type, price", INGREDIENT_TEST_CASES)
+    def test_get_name(self, name, type, price):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_name() == name
 
-@pytest.mark.parametrize("name, type, price", INGREDIENT_TEST_CASES)
-def test_get_name(name, type, price):
-    ingredient = Ingredient(type, name, price)
-    assert ingredient.get_name() == name
+    @pytest.mark.parametrize("name, type, price", INGREDIENT_TEST_CASES)
+    def test_get_price(self, name, type, price):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_price() == price
 
-@pytest.mark.parametrize("name, type, price", INGREDIENT_TEST_CASES)
-def test_get_price(name, type, price):
-    ingredient = Ingredient(type, name, price)
-    assert ingredient.get_price() == price
-
-@pytest.mark.parametrize("name, type, price", INGREDIENT_TEST_CASES)
-def test_get_type(name, type, price):
-    ingredient = Ingredient(type, name, price)
-    assert ingredient.get_type() == type
+    @pytest.mark.parametrize("name, type, price", INGREDIENT_TEST_CASES)
+    def test_get_type(self, name, type, price):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_type() == type
