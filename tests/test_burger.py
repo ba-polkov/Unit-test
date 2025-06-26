b58d5573d80d7539a9017053aa57e741e4dccef5
Andrei Borodin <andrei.borodin@dcsportsoft.com>
2025-06-26 17:51:41 +0300

    Change test_burger

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,9 +1,5 @@
-
 import pytest
-from praktikum.bun import Bun
 from praktikum.burger import Burger
-from praktikum.ingredient import Ingredient
-from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 
 class TestBurger:
