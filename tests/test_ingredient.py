ed12ff192c694ac933ea5b3f37d1a745fab5fce8
somanytearsss <somanytearsss@yandex.ru>
2025-07-09 18:18:45 +0300

    Update test_ingredient.py

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -6,7 +6,7 @@ from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FI
     (INGREDIENT_TYPE_FILLING, 'big naggets', 250.20),
     (INGREDIENT_TYPE_SAUCE, 'garlic sauce', 95.00)
 ])
-def test_igredient_init(ingredient_type,name, price):
+def test_ingredient_init(ingredient_type,name, price):
     ing = Ingredient(ingredient_type,name,price)
     assert ing.get_type() == ingredient_type
     assert ing.get_name() == name
