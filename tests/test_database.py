9cbfa9be6f16008846db946adcdf54e11829e2e1
somanytearsss <somanytearsss@yandex.ru>
2025-07-09 18:14:33 +0300

    tests

---
+++ tests/test_database.py
@@ -0,0 +1,9 @@
+class TestDataBase:
+
+    def test_available_buns(self, database):
+        buns = database.available_buns()
+        assert len(buns) == 3
+
+    def test_available_ingredients(self, database):
+        ingredients = database.available_ingredients()
+        assert len(ingredients) == 6
---
+++ tests/test_ingredient.py
@@ -0,0 +1,13 @@
+import pytest
+from praktikum.ingredient import Ingredient
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+@pytest.mark.parametrize('ingredient_type, name, price', [
+    (INGREDIENT_TYPE_FILLING, 'big naggets', 250.20),
+    (INGREDIENT_TYPE_SAUCE, 'garlic sauce', 95.00)
+])
+def test_igredient_init(ingredient_type,name, price):
+    ing = Ingredient(ingredient_type,name,price)
+    assert ing.get_type() == ingredient_type
+    assert ing.get_name() == name
+    assert ing.get_price() == price
