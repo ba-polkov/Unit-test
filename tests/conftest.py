ceb031ba491b4300ff672442c2066655d9ee3346
somanytearsss <somanytearsss@yandex.ru>
2025-07-19 16:25:29 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,6 @@
 from unittest.mock import Mock
-
 import pytest
 
-from praktikum.burger import Burger
-from praktikum.database import Database
-
-
-@pytest.fixture()
-def burger():
-    return Burger()
 
 @pytest.fixture()
 def bun_mock():
@@ -24,6 +16,7 @@ def ingredient_mock():
     ingredient_mock.get_name.return_value = 'test sause'
     ingredient_mock.get_price.return_value = 22.0
     return ingredient_mock
+
 @pytest.fixture()
 def other_ingredient_mock():
     other_ingredient_mock = Mock()
@@ -31,7 +24,3 @@ def other_ingredient_mock():
     other_ingredient_mock.get_name.return_value = 'test filling'
     other_ingredient_mock.get_price.return_value = 42.0
     return other_ingredient_mock
-
-@pytest.fixture()
-def database():
-    return Database()
