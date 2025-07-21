0a79860471044230a35aa6e468fd5590acfaa258
Elena Ermolenkova <e-wolf@rambler.ru>
2025-07-21 21:16:38 +0300

    Убрала фикстуры, создала объекты в тестах. Убрала отладочный принт.

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,14 +1,27 @@
+from unittest.mock import Mock
+from praktikum.burger import Burger
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
 class TestBurger:
 
     # Тест установки булки
-    def test_set_buns(self, burger, mock_bun):
+    def test_set_buns(self):
+        burger = Burger()
+        mock_bun = Mock()
+        mock_bun.get_name.return_value = 'Булочка'
+        mock_bun.get_price.return_value = 100
         burger.set_buns(mock_bun)
         assert burger.bun == mock_bun
         assert burger.bun.get_name() == 'Булочка'
         assert burger.bun.get_price() == 100
 
     # Тест добавления ингредиента
-    def test_add_ingredient(self, burger, mock_ingredient_one):
+    def test_add_ingredient(self):
+        burger = Burger()
+        mock_ingredient_one = Mock()
+        mock_ingredient_one.get_type.return_value = INGREDIENT_TYPE_FILLING
+        mock_ingredient_one.get_name.return_value = 'Курица'
+        mock_ingredient_one.get_price.return_value = 250
         burger.add_ingredient(mock_ingredient_one)
         assert len(burger.ingredients) == 1
         assert burger.ingredients[0] == mock_ingredient_one
@@ -16,13 +29,27 @@ class TestBurger:
         assert burger.ingredients[-1].get_price() == 250
 
     # Тест удаления ингредиента
-    def test_remove_ingredient(self, burger, mock_ingredient_one):
+    def test_remove_ingredient(self):
+        burger = Burger()
+        mock_ingredient_one = Mock()
+        mock_ingredient_one.get_type.return_value = INGREDIENT_TYPE_FILLING
+        mock_ingredient_one.get_name.return_value = 'Курица'
+        mock_ingredient_one.get_price.return_value = 250
         burger.add_ingredient(mock_ingredient_one)
         burger.remove_ingredient(0)
         assert len(burger.ingredients) == 0
 
     # Тест перемещения ингредиента
-    def test_move_ingredient(self, burger, mock_ingredient_one, mock_ingredient_two):
+    def test_move_ingredient(self):
+        burger = Burger()
+        mock_ingredient_one = Mock()
+        mock_ingredient_one .get_type.return_value = INGREDIENT_TYPE_FILLING
+        mock_ingredient_one .get_name.return_value = 'Курица'
+        mock_ingredient_one .get_price.return_value = 250
+        mock_ingredient_two = Mock()
+        mock_ingredient_two.get_type.return_value = INGREDIENT_TYPE_SAUCE
+        mock_ingredient_two.get_name.return_value = 'Кетчуп'
+        mock_ingredient_two.get_price.return_value = 90
         burger.add_ingredient(mock_ingredient_one)
         burger.add_ingredient(mock_ingredient_two)
         burger.move_ingredient(0, 1)
@@ -30,7 +57,19 @@ class TestBurger:
         assert burger.ingredients[1] == mock_ingredient_one
 
     # Тест расчета цены бургера
-    def test_get_price(self, burger, mock_bun, mock_ingredient_one, mock_ingredient_two):
+    def test_get_price(self):
+        burger = Burger()
+        mock_bun = Mock()
+        mock_bun.get_name.return_value = 'Булочка'
+        mock_bun.get_price.return_value = 100
+        mock_ingredient_one = Mock()
+        mock_ingredient_one.get_type.return_value = INGREDIENT_TYPE_FILLING
+        mock_ingredient_one.get_name.return_value = 'Курица'
+        mock_ingredient_one.get_price.return_value = 250
+        mock_ingredient_two = Mock()
+        mock_ingredient_two.get_type.return_value = INGREDIENT_TYPE_SAUCE
+        mock_ingredient_two.get_name.return_value = 'Кетчуп'
+        mock_ingredient_two.get_price.return_value = 90
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient_one)
         burger.add_ingredient(mock_ingredient_two)
@@ -38,7 +77,19 @@ class TestBurger:
         assert burger.get_price() == 540
 
     # Тест формирования чека
-    def test_get_receipt(self, burger, mock_bun, mock_ingredient_one, mock_ingredient_two):
+    def test_get_receipt(self):
+        burger = Burger()
+        mock_bun = Mock()
+        mock_bun.get_name.return_value = 'Булочка'
+        mock_bun.get_price.return_value = 100
+        mock_ingredient_one = Mock()
+        mock_ingredient_one.get_type.return_value = INGREDIENT_TYPE_FILLING
+        mock_ingredient_one.get_name.return_value = 'Курица'
+        mock_ingredient_one.get_price.return_value = 250
+        mock_ingredient_two = Mock()
+        mock_ingredient_two.get_type.return_value = INGREDIENT_TYPE_SAUCE
+        mock_ingredient_two.get_name.return_value = 'Кетчуп'
+        mock_ingredient_two.get_price.return_value = 90
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient_one)
         burger.add_ingredient(mock_ingredient_two)
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,27 +1,30 @@
 import pytest
 from typing import List
+from praktikum.database import Database
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
+
 class TestDatabase:
+    database = Database()
 
     # Тест на проверку количества булочек
-    def test_count_available_buns(self, database):
-        buns = database.available_buns()
+    def test_count_available_buns(self):
+        buns = self.database.available_buns()
         assert len(buns) == 3
 
     # Тест на проверку количества ингредиентов
-    def test_count_available_ingredients(self, database):
-        assert len(database.available_ingredients()) == 6
+    def test_count_available_ingredients(self):
+        assert len(self.database.available_ingredients()) == 6
 
     # Тест на проверку количества соусов
-    def test_count_available_sauces(self, database):
-        ingredients = database.available_ingredients()
+    def test_count_available_sauces(self):
+        ingredients = self.database.available_ingredients()
         sauces = [ingredient for ingredient in ingredients if ingredient.type == INGREDIENT_TYPE_SAUCE]
         assert len(sauces) == 3
 
     # Тест на проверку количества начинок
-    def test_count_available_fillings(self, database):
-        ingredients = database.available_ingredients()
+    def test_count_available_fillings(self):
+        ingredients = self.database.available_ingredients()
         fillings = [ingredient for ingredient in ingredients if ingredient.type == INGREDIENT_TYPE_FILLING]
         assert len(fillings) == 3
 
@@ -31,8 +34,8 @@ class TestDatabase:
         (1, 'white bun', 200),
         (2, 'red bun', 300)
         ])
-    def test_correct_data_buns(self, database, index, expected_name, expected_price):
-        buns = database.available_buns()
+    def test_correct_data_buns(self, index, expected_name, expected_price):
+        buns = self.database.available_buns()
         assert buns[index].get_name() == expected_name
         assert buns[index].get_price() == expected_price
 
@@ -45,16 +48,16 @@ class TestDatabase:
             (4, INGREDIENT_TYPE_FILLING, 'dinosaur', 200),
             (5, INGREDIENT_TYPE_FILLING, 'sausage', 300),
             ])
-    def test_correct_data_ingredients(self, database, index, expected_type, expected_name, expected_price):
-        ingredients = database.available_ingredients()
+    def test_correct_data_ingredients(self, index, expected_type, expected_name, expected_price):
+        ingredients = self.database.available_ingredients()
         assert ingredients[index].get_type() == expected_type
         assert ingredients[index].get_name() == expected_name
         assert ingredients[index].get_price() == expected_price
 
     # Тест на проверку возвращения списка булочек
-    def test_returns_list_available_buns(self, database):
-        assert isinstance(database.available_buns(), List)
+    def test_returns_list_available_buns(self):
+        assert isinstance(self.database.available_buns(), List)
 
     # Тест на проверку возвращения списка ингредиентов
-    def test_returns_list_available_ingredients(self, database):
-        assert isinstance(database.available_ingredients(), List)
+    def test_returns_list_available_ingredients(self):
+        assert isinstance(self.database.available_ingredients(), List)
