afa66a59ae708af6c9b4fce94b20d804f5b73dac
dea-1611 <dea.ws1611@gmail.com>
2025-08-19 18:07:28 +0500

    Update test_ingredient.py

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,21 +1,32 @@
-import pytest  # Добавляем импорт pytest
 import allure
+import pytest
+from praktikum.ingredient import Ingredient
+from data import SAUCE_TYPE, SAUCE_NAME, SAUCE_PRICE, FILLING_TYPE, FILLING_NAME, FILLING_PRICE
 
 class TestIngredient:
     @allure.title('Получение типа ингредиента')
-    @pytest.mark.parametrize('ingredient_type', ['sauce', 'filling'])
-    def test_get_type(self, ingredient_type, request, ingredient_data):
-        mock_ingredient = request.getfixturevalue(f'mock_{ingredient_type}')
-        assert mock_ingredient.get_type() == ingredient_data[ingredient_type]['type']
+    @pytest.mark.parametrize('ingredient_type, name, price', [
+        (SAUCE_TYPE, SAUCE_NAME, SAUCE_PRICE),
+        (FILLING_TYPE, FILLING_NAME, FILLING_PRICE)
+    ])
+    def test_get_type(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_type() == ingredient_type
 
     @allure.title('Получение названия ингредиента')
-    @pytest.mark.parametrize('ingredient_type', ['sauce', 'filling'])
-    def test_get_name(self, ingredient_type, request, ingredient_data):
-        mock_ingredient = request.getfixturevalue(f'mock_{ingredient_type}')
-        assert mock_ingredient.get_name() == ingredient_data[ingredient_type]['name']
+    @pytest.mark.parametrize('ingredient_type, name, price', [
+        (SAUCE_TYPE, SAUCE_NAME, SAUCE_PRICE),
+        (FILLING_TYPE, FILLING_NAME, FILLING_PRICE)
+    ])
+    def test_get_name(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_name() == name
 
     @allure.title('Получение цены ингредиента')
-    @pytest.mark.parametrize('ingredient_type', ['sauce', 'filling'])
-    def test_get_price(self, ingredient_type, request, ingredient_data):
-        mock_ingredient = request.getfixturevalue(f'mock_{ingredient_type}')
-        assert mock_ingredient.get_price() == ingredient_data[ingredient_type]['price']
+    @pytest.mark.parametrize('ingredient_type, name, price', [
+        (SAUCE_TYPE, SAUCE_NAME, SAUCE_PRICE),
+        (FILLING_TYPE, FILLING_NAME, FILLING_PRICE)
+    ])
+    def test_get_price(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_price() == price
