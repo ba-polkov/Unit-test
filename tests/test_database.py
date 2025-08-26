0dd78c957ef0b23ab0bd154644206570edd8df5d
Алексей Заикин <zaikinalex@inbox.ru>
2025-08-26 11:13:41 +0300

    уточнение тестов Database

--- tests/test_database.py
+++ tests/test_database.py
@@ -10,22 +10,33 @@ class TestDatabase:
         ('black bun', 1), ('white bun', 1), ('red bun', 1),
         ('brown bun', 0), ('snowy bun', 0),
         ])
-    def test_available_buns_items(self, database, name, expected_count):
+    def test_available_buns_set(self, database, name, expected_count):
         buns = database.available_buns()
         count = len(list(filter(lambda e: e.name == name, buns)))
         assert count == expected_count
+
+    def test_available_buns_prices(self, database):
+        buns = database.available_buns()
+        assert all(filter(lambda bun: bun.price > 0, buns))
 
     @pytest.mark.parametrize('type, name, expected_count',
         [
             (INGREDIENT_TYPE_SAUCE, 'hot sauce', 1),
             (INGREDIENT_TYPE_SAUCE, 'sour cream', 1),
+            (INGREDIENT_TYPE_SAUCE, 'chili sauce', 1),
             (INGREDIENT_TYPE_SAUCE, 'dummy sauce', 0),
             (INGREDIENT_TYPE_FILLING, 'cutlet', 1),
             (INGREDIENT_TYPE_FILLING, 'dinosaur', 1),
+            (INGREDIENT_TYPE_FILLING, 'sausage', 1),
             (INGREDIENT_TYPE_FILLING, 'no name', 0),
             ('WRONG TYPE', 'cutlet', 0),
         ])
-    def test_available_ingredients_items(self, database, type, name, expected_count):
+    def test_available_ingredients_set(self, database, type, name, expected_count):
         ingredients = database.available_ingredients()
         count = len(list(filter(lambda e: e.type == type and e.name == name, ingredients)))
         assert count == expected_count
+
+    def test_available_ingredients_prices(self, database):
+        ingredients = database.available_ingredients()
+        count = all(filter(lambda ingredient: ingredient.price > 0, ingredients))
+
