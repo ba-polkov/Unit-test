79e01a27c48e1743a23a027f5310e24d718ac474
Vlad Potapov <vlad1080@yandex.ru>
2025-10-02 18:02:36 +0300

    Upd 2

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -6,6 +6,8 @@ from tests.data import TestData
 
 
 class TestBurger:
+
+    # Позитивные тесты
     def test_set_buns(self):
         """Тестирование метода выбора булки"""
         burger = Burger()
@@ -14,12 +16,6 @@ class TestBurger:
         mock_bun.get_price.return_value = TestData.buns[0][1]
         burger.set_buns(mock_bun)
         assert burger.bun == mock_bun
-
-    def test_set_buns_with_none(self):
-        """Негативный тест: установка булки как None"""
-        burger = Burger()
-        burger.set_buns(None)
-        assert burger.bun is None
 
     def test_add_ingredient(self):
         """Тестирование метода добавления ингредиента"""
@@ -31,12 +27,6 @@ class TestBurger:
         burger.add_ingredient(mock_ingredient)
         assert burger.ingredients == [mock_ingredient]
 
-    def test_add_ingredient_none(self):
-        """Негативный тест: добавление None как ингредиента"""
-        burger = Burger()
-        burger.add_ingredient(None)
-        assert burger.ingredients == [None]
-
     def test_remove_ingredient(self):
         """Тестирование метода удаления ингредиента"""
         burger = Burger()
@@ -47,24 +37,6 @@ class TestBurger:
         burger.add_ingredient(mock_ingredient)
         burger.remove_ingredient(0)
         assert burger.ingredients == []
-
-    def test_remove_ingredient_invalid_index(self):
-        """Негативный тест: удаление с неверным индексом"""
-        burger = Burger()
-        mock_ingredient = Mock()
-        burger.add_ingredient(mock_ingredient)
-
-        with pytest.raises(IndexError):
-            burger.remove_ingredient(5)
-
-    def test_remove_ingredient_negative_index(self):
-        """Негативный тест: удаление с отрицательным индексом"""
-        burger = Burger()
-        mock_ingredient = Mock()
-        burger.add_ingredient(mock_ingredient)
-
-        with pytest.raises(IndexError):
-            burger.remove_ingredient(-2)
 
     def test_move_ingredient(self):
         """Тестирование метода перемещения ингредиента"""
@@ -82,25 +54,6 @@ class TestBurger:
         assert burger.ingredients[0] == mock_ingredient2
         assert burger.ingredients[1] == mock_ingredient3
         assert burger.ingredients[2] == mock_ingredient1
-
-    def test_move_ingredient_invalid_index(self):
-        """Негативный тест: перемещение с неверным индексом"""
-        burger = Burger()
-        mock_ingredient = Mock()
-        burger.add_ingredient(mock_ingredient)
-
-        with pytest.raises(IndexError):
-            burger.move_ingredient(5, 0)
-
-    def test_move_ingredient_same_index(self):
-        """Негативный тест: перемещение на ту же позицию"""
-        burger = Burger()
-        mock_ingredient = Mock()
-        burger.add_ingredient(mock_ingredient)
-
-        original_ingredients = burger.ingredients.copy()
-        burger.move_ingredient(0, 0)
-        assert burger.ingredients == original_ingredients
 
     def test_get_price(self):
         """Тестирование метода получения цены"""
@@ -125,41 +78,26 @@ class TestBurger:
         )
         assert burger.get_price() == expected_price
 
-    def test_get_price_without_bun(self):
-        """Негативный тест: получение цены без булки"""
-        burger = Burger()
-        mock_ingredient = Mock()
-        mock_ingredient.get_price.return_value = 100
-        burger.add_ingredient(mock_ingredient)
-
-        with pytest.raises(AttributeError):
-            burger.get_price()
-
-    def test_get_price_empty_burger(self):
-        """Негативный тест: получение цены пустого бургера"""
-        burger = Burger()
-        mock_bun = Mock()
-        mock_bun.get_price.return_value = 100
-        burger.set_buns(mock_bun)
-
-        assert burger.get_price() == 200  # 100 * 2
-
     def test_get_receipt(self):
         """Тестирование метода получения чека"""
         burger = Burger()
         mock_bun = Mock()
         mock_bun.get_name.return_value = TestData.buns[1][0]
-        mock_bun.get_price.return_value = TestData.buns[1][1]
+        mock_bun.get_price.return_value = TestData.buns[1][1]  # Добавлен return_value
 
         mock_ingredient1 = Mock()
         mock_ingredient1.get_type.return_value = INGREDIENT_TYPE_FILLING
         mock_ingredient1.get_name.return_value = TestData.ingredients[0][1]
-        mock_ingredient1.get_price.return_value = TestData.ingredients[0][2]
+        mock_ingredient1.get_price.return_value = TestData.ingredients[0][
+            2
+        ]  # Добавлен return_value
 
         mock_ingredient2 = Mock()
         mock_ingredient2.get_type.return_value = INGREDIENT_TYPE_SAUCE
         mock_ingredient2.get_name.return_value = TestData.ingredients[6][1]
-        mock_ingredient2.get_price.return_value = TestData.ingredients[6][2]
+        mock_ingredient2.get_price.return_value = TestData.ingredients[6][
+            2
+        ]  # Добавлен return_value
 
         burger.set_buns(mock_bun)
         burger.add_ingredient(mock_ingredient1)
@@ -174,8 +112,19 @@ class TestBurger:
         assert "sauce" in receipt.lower()
         assert "Price:" in receipt
 
-    def test_get_receipt_without_bun(self):
-        """Негативный тест: получение чека без булки"""
+    # Граничные случаи и негативные тесты
+    def test_get_price_without_bun_raises_error(self):
+        """Тест: получение цены без булки вызывает ошибку"""
+        burger = Burger()
+        mock_ingredient = Mock()
+        mock_ingredient.get_price.return_value = 100
+        burger.add_ingredient(mock_ingredient)
+
+        with pytest.raises(AttributeError):
+            burger.get_price()
+
+    def test_get_receipt_without_bun_raises_error(self):
+        """Тест: получение чека без булки вызывает ошибку"""
         burger = Burger()
         mock_ingredient = Mock()
         mock_ingredient.get_name.return_value = "Test Ingredient"
@@ -185,14 +134,50 @@ class TestBurger:
         with pytest.raises(AttributeError):
             burger.get_receipt()
 
-    def test_get_receipt_empty_burger(self):
-        """Негативный тест: получение чека бургера только с булкой"""
+    def test_remove_ingredient_invalid_index_raises_error(self):
+        """Тест: удаление с неверным индексом вызывает ошибку"""
+        burger = Burger()
+        mock_ingredient = Mock()
+        burger.add_ingredient(mock_ingredient)
+
+        with pytest.raises(IndexError):
+            burger.remove_ingredient(5)
+
+    def test_move_ingredient_invalid_index_raises_error(self):
+        """Тест: перемещение с неверным индексом вызывает ошибку"""
+        burger = Burger()
+        mock_ingredient = Mock()
+        burger.add_ingredient(mock_ingredient)
+
+        with pytest.raises(IndexError):
+            burger.move_ingredient(5, 0)
+
+    def test_get_price_empty_burger_only_bun(self):
+        """Тест: цена бургера только с булкой"""
         burger = Burger()
         mock_bun = Mock()
-        mock_bun.get_name.return_value = "Test Bun"
         mock_bun.get_price.return_value = 100
         burger.set_buns(mock_bun)
 
+        assert burger.get_price() == 200
+
+    def test_get_receipt_single_ingredient(self):
+        """Тест: чек бургера с одним ингредиентом"""
+        burger = Burger()
+        mock_bun = Mock()
+        mock_bun.get_name.return_value = "Test Bun"
+        mock_bun.get_price.return_value = 100  
+
+        mock_ingredient = Mock()
+        mock_ingredient.get_type.return_value = INGREDIENT_TYPE_FILLING
+        mock_ingredient.get_name.return_value = "Test Filling"
+        mock_ingredient.get_price.return_value = 50  
+
+        burger.set_buns(mock_bun)
+        burger.add_ingredient(mock_ingredient)
+
         receipt = burger.get_receipt()
         assert "Test Bun" in receipt
-        assert "Price: 200" in receipt
+        assert "Test Filling" in receipt
+        assert "filling" in receipt.lower()
+        assert "Price:" in receipt
